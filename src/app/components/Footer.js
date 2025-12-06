af6d625fdd796390353468c8038fc934959aa696
TomasSueno <sleboda2008@gmail.com>
2025-12-06 07:24:46 +0100

    database

---
+++ src/app/components/Footer.js
@@ -0,0 +1,11 @@
+"use client"
+
+export default function Footer() {
+    return (
+        <>
+            <div>
+                <p>All rights reserved</p>
+            </div>
+        </>
+    )
+}
