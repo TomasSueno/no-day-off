28e56dd0688e4b73d259a97cdc69edca96ae49a7
TomasSueno <sleboda2008@gmail.com>
2025-12-09 08:36:07 +0100

    database experiment

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,19 @@
 import { supabase } from "./lib/supabase";
 
 export default async function Home() {
-  const { data } = await supabase.from("users").select("*");
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  const setNewView = async () => {
+      const { data } = await supabase
+  .from("new_users")
+  .insert({
+    name: "random_name"
+  });
+
+  if (data) console.log(data)
+  }
+
+  setNewView()
+
+
+  // return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return <div>Hello!</div>
 }
